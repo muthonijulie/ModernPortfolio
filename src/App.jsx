47f3e79701dca8a6f60283e23a7a2a2803ba4ae4
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2025-04-06 18:39:18 +0300

    My modern portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Portfolio from './components/Portfolio'
+
+function App() {
+  return (
+    <div >
+      <Portfolio/> 
+    </div>
+  );
+};
+
+export default App;
